5c2aa62ffd1bfc2ab28b9aa0a0f960027855960a
NikolaOd <nikola.odalovic92@gmail.com>
2023-01-19 02:39:34 +0100

    Set up tailwind and Prisma.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,15 @@
-import './globals.css'
+import "./globals.css";
+import Header from "./header";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html data-theme="cupcake">
       <head />
-      <body>{children}</body>
+      <body>
+        {/* @ts-expect-error Server Component */}
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
