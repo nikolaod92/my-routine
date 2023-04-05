dcc2cd5b0bdd7bf8e1bbd34eec8bab577b03f84f
NikolaOd <nikola.odalovic92@gmail.com>
2023-04-05 22:55:47 +0200

    Change routine info form styles

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -6,7 +6,7 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
 import { useStore } from '@/store'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { RoutineInfo, routineSchema } from '@/lib/vaildators'
+import { RoutineInfo, routineSchema } from '@/lib/validators'
 
 export default function Create() {
   const router = useRouter()
@@ -20,7 +20,7 @@ export default function Create() {
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting },
+    formState: { errors },
   } = useForm({
     defaultValues: {
       ...routineInfo,
@@ -35,41 +35,48 @@ export default function Create() {
   }
 
   return (
-    <div className="card card-body bg-base-100 rounded shadow max-w-sm mx-auto">
-      <form onSubmit={handleSubmit(onSubmit)} className="form-control">
-        <h1 className="text-xl font-bold mb-4">Create a routine</h1>
-        <label className="label">
-          <span className="label-text">Name</span>
-        </label>
-        <input
-          type="text"
-          className="input input-bordered input-sm w-full"
-          {...register('name')}
-        />
-        {errors.name && (
-          <p className="text-xs italic text-red-500 mt-2">
-            {errors.name?.message}
-          </p>
-        )}
-        <label className="label">
-          <span className="label-text">Description</span>
-        </label>
-        <textarea
-          className="textarea textarea-bordered textarea-md"
-          {...register('description')}
-        />
-        {errors.description && (
-          <p className="text-xs italic text-red-500 mt-2">
-            {errors.description?.message}
-          </p>
-        )}
-        <button
-          type="submit"
-          disabled={isSubmitting}
-          className={`btn btn-primary mt-6 self-end  ${
-            isSubmitting && 'loading'
-          }`}
-        >
+    <div className="card card-body mx-auto max-w-md rounded-lg border-t-4 border-t-primary bg-base-100 shadow">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="form-control space-y-4"
+      >
+        <h1 className="text-2xl font-bold">Create a routine</h1>
+        <div>
+          <label htmlFor="name" className="label">
+            <span className="text-xs font-semibold uppercase leading-3">
+              Name
+            </span>
+          </label>
+          <input
+            id="name"
+            type="text"
+            className="input input-sm w-full bg-primary/10 text-lg font-medium"
+            {...register('name')}
+          />
+          {errors.name && (
+            <p className="mt-1 text-xs italic text-red-500">
+              {errors.name?.message}
+            </p>
+          )}
+        </div>
+        <div>
+          <label htmlFor="description" className="label">
+            <span className="text-xs font-semibold uppercase leading-3">
+              Description
+            </span>
+          </label>
+          <textarea
+            id="description"
+            className="textarea h-32 w-full bg-primary/10 px-4 py-2 text-sm font-medium"
+            {...register('description')}
+          />
+          {errors.description && (
+            <p className="text-xs italic text-red-500">
+              {errors.description?.message}
+            </p>
+          )}
+        </div>
+        <button type="submit" className="btn-primary btn-sm btn self-end">
           Next
         </button>
       </form>
