94757a8b1edf1f55beb6e9e83a07804d4f14d078
NikolaOd <nikola.odalovic92@gmail.com>
2023-04-06 00:48:51 +0200

    Change redirect URL

--- components/SignIn/SignInButton.tsx
+++ components/SignIn/SignInButton.tsx
@@ -9,7 +9,7 @@ function SignInButton() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: '/',
+        redirectTo: window.location.origin,
       },
     })
   }
