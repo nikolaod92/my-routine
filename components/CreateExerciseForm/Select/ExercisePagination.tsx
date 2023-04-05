f4ad21552b8b47ebbc8183baddf14196121ae811
NikolaOd <nikola.odalovic92@gmail.com>
2023-04-05 22:30:37 +0200

    Update responsive pagination buttons

--- components/CreateExerciseForm/Select/ExercisePagination.tsx
+++ components/CreateExerciseForm/Select/ExercisePagination.tsx
@@ -14,19 +14,19 @@ function ExercisePagination({ range, setRange, count }: Props) {
     <div className="btn-group">
       {range > 0 && (
         <button
-          className="btn btn-xs"
+          className="btn btn-xs md:btn-sm"
           onClick={() => setRange((prev) => prev - PAGINATION_STEP)}
         >
           «
         </button>
       )}
-      <button className="btn btn-xs">
+      <button className="btn btn-xs md:btn-sm">
         Page {(range + PAGINATION_STEP) / PAGINATION_STEP}
       </button>
       {count - (range + 1) > PAGINATION_STEP && (
         <button
           onClick={() => setRange((prev) => prev + PAGINATION_STEP)}
-          className="btn btn-xs"
+          className="btn btn-xs md:btn-sm"
         >
           »
         </button>
