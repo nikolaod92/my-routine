79b7d93856f0f8f10e0c6abbdc31c91e89bc5309
NikolaOd <nikola.odalovic92@gmail.com>
2023-04-05 22:49:16 +0200

    Animate component, add reset button

--- components/CreateExerciseForm/SelectedExerciseDisplay.tsx
+++ components/CreateExerciseForm/SelectedExerciseDisplay.tsx
@@ -4,6 +4,8 @@ import { useStore } from '@/store'
 import { useMemo } from 'react'
 import { TrashIcon } from '@heroicons/react/20/solid'
 import { AnimatePresence, motion } from 'framer-motion'
+import ResetButton from './ResetButton'
+import SubmitRoutine from './SubmitRoutine'
 
 function SelectedExerciseDisplay() {
   const [exercises, currentDay, deleteExercise] = useStore((state) => [
@@ -18,17 +20,25 @@ function SelectedExerciseDisplay() {
   )
 
   return (
-    <div className="flex flex-col my-2 rounded shadow-sm overflow-hidden mb-6">
+    <div className="flex flex-col mt-2 rounded overflow-hidden mb-4">
+      {!exercisesOnDay.length && (
+        <motion.div
+          animate={{ y: [-10, 0] }}
+          className="text-xs bg-base-100 px-2 py-1"
+        >
+          No exercises selected for {currentDay}.
+        </motion.div>
+      )}
       <AnimatePresence initial={false} mode="popLayout">
         {exercisesOnDay.map((ex) => (
           <motion.div
             initial={{ opacity: 0 }}
-            animate={{ opacity: 1, x: [50, 0] }}
-            exit={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, y: [-10, 0] }}
+            exit={{ opacity: 0, y: -10 }}
             key={ex.exercise_id}
             className="flex justify-between"
           >
-            <div className="flex flex-1 justify-between capitalize text-xs bg-base-100  px-2 py-1 ">
+            <div className="flex flex-1 justify-between capitalize text-xs bg-base-100 px-2 py-1 ">
               <h2 className=" font-semibold line-clamp-1">{ex.name}</h2>
               <div className="flex space-x-2">
                 <p className="badge badge-sm badge-primary font-semibold text-base-100 whitespace-nowrap">
@@ -46,6 +56,12 @@ function SelectedExerciseDisplay() {
           </motion.div>
         ))}
       </AnimatePresence>
+      {exercises.length ? (
+        <div className="flex items-center justify-center space-x-2 self-end mt-2">
+          <ResetButton />
+          <SubmitRoutine />
+        </div>
+      ) : null}
     </div>
   )
 }
