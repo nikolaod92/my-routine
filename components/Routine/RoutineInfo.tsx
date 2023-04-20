1c41a900d1357131615037dfb46625499b40678c
NikolaOd <nikola.odalovic92@gmail.com>
2023-04-20 23:42:09 +0200

    Remove follower count from routine info

--- components/Routine/RoutineInfo.tsx
+++ components/Routine/RoutineInfo.tsx
@@ -1,22 +1,14 @@
 import { Routine } from '@/lib/database.types'
 import FollowRoutineButton from './FollowRoutineButton'
-import FollowerCount from './FollowerCount'
 
 function RoutineInfo({ routine }: { routine: Routine }) {
   return (
     <div>
       <div className="flex justify-between items-center">
-        <div className="flex items-center">
-          <h1 className="font-bold text-2xl md:text-3xl mr-2 truncate">
-            {routine?.name}
-          </h1>
-          <FollowerCount count={routine.follower_count} />
-        </div>
+        <h1 className="font-bold text-3xl truncate">{routine?.name}</h1>
         <FollowRoutineButton id={routine.id} />
       </div>
-      <h1 className="font-medium leading-5 text-md line-clamp-3 mt-1">
-        {routine?.description}
-      </h1>
+      <h1 className="font-medium leading-5 text-md">{routine?.description}</h1>
     </div>
   )
 }
